edd0743931f020838c660769bbc95fa92fb90210
mirkop <mirko85.dev@gmail.com>
2025-11-17 10:28:59 +0100

    update login

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Facades\Filament;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Symfony\Component\HttpFoundation\Response;
 
 class User extends Authenticatable  implements FilamentUser
 {
@@ -57,5 +59,19 @@ class User extends Authenticatable  implements FilamentUser
         if ($panel->getId() === 'admin') { return $this->hasRole('super_admin'); }          // solo chi è ha ruolo super_admin può accedere al panel amministrazione
         if ($panel->getId() === 'user') { return true; }                                    // tutti gli utenti possono accedere al panel operatore
         return false;
+    }
+
+        public function loginRedirect(): ?Response
+    {
+        $destinationPanelId = null;
+        if ($this->hasRole('super_admin'))
+            $destinationPanelId = 'admin';
+        else
+            $destinationPanelId = 'user';
+        
+        if (!$destinationPanelId)
+            return abort(403, 'Accesso non autorizzato a nessun pannello.');
+        
+        return redirect()->to(Filament::getPanel($destinationPanelId)->getUrl());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SsoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/callback', [SsoController::class, 'callback'])->name('sso.callback');
+Route::get('/sso-login', [SsoController::class, 'redirect'])->name('sso.login');
+
+Route::get('/admin/login', fn() => redirect()->route('sso.login'))->name('filament.admin.auth.login');
+
+Route::get('/login', fn() => redirect()->route('sso.login'));
+
