a40befca31a1b63e38c0d2f2316215be2ef912ca
progSarida <programmazione@sarida.it>
2025-11-26 14:17:37 +0100

    Correzione

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -46,7 +46,7 @@ class UserPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/User/Widgets'), for: 'App\\Filament\\User\\Widgets')
             ->widgets([
-                // Widgets\AccountWidget::class,
+                Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
